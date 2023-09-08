7256a32be2e723e53570f54f98f5c6a4230b5b91
Kingsley Mwaura <njungeking@gmail.com>
2023-09-07 23:50:09 -0700

    Got value of EOF

---
+++ cio/eof.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the value of EOF
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c = EOF;
+	{
+		printf("%d\n", c);
+	}
+}
