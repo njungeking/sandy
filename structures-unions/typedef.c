8329a892ba970e8db3725e4da87e6cd1ca08523f
Kingsley Mwaura <njungeking@gmail.com>
2024-01-07 13:38:52 +0300

    Typedef

---
+++ structures-unions/typedef.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*Typedef*/
+
+int main()
+{
+    typedef int digits;
+    digits x,y,z;
+    x=10,y=20,z=30;
+    printf("The value of x is %d.\n\n",x);
+    printf("The value of y is %d.\n\n",y);
+    printf("The value of z is %d.\n\n",z);
+}
+
