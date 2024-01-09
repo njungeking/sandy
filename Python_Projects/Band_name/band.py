e7e0738553670c1ba27e158535c377c0a585b0a2
Kingsley Mwaura <njungeking@gmail.com>
2024-01-09 21:02:06 +0300

    Band name Project complete

---
+++ Python_Projects/Band_name/band.py
@@ -0,0 +1,6 @@
+# Project Band name generator.
+
+print("Welcome to a Band name generator program!\n")
+city=input("Which City did you grow up in?\n")
+pet=input("What is the name of your pet?\n")
+print("Your Band name is: "+city+" "+pet+".")
---
+++ Python_Projects/Band_name/len.py
@@ -0,0 +1,5 @@
+# Determining the length of a string.
+
+name = input("What is your name? \n")
+length=len(name)
+print(length)
