6229b0d4b7bb1cf95630b4b93b2f3d2c5870f7d7
Kingsley Mwaura <njungeking@gmail.com>
2024-01-10 17:38:49 +0300

    Type conversion and data types

---
+++ Python_Projects/Band_name/type-conv.py
@@ -0,0 +1,7 @@
+# Data types including type conversion
+
+two_digit_number=input("Type a two digit number: \n")
+dig1=two_digit_number[0]
+dig2=two_digit_number[1]
+sum=int(dig1)+int(dig2)
+print(sum)
