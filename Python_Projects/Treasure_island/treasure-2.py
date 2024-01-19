de5a063862a18c4cfe65db1f9348aaec78f5c4db
Kingsley Mwaura <njungeking@gmail.com>
2024-01-19 19:54:38 +0300

    Another go on the Treasure island project

---
+++ Python_Projects/Treasure_island/treasure-2.py
@@ -0,0 +1,25 @@
+# Treasure island project.
+
+print("Welcome to treasure island!\n")
+print("Your mission is to find the treasure!\n")
+
+dir=input("Which direction would you like to \n"
+         "go to: 'Left' or 'Right'? \n")
+dir_low=dir.lower()
+if dir_low == "left":
+  opt=input("Would you like to 'Swim' or 'Wait'? \n")
+  opt_low=opt.lower()
+  if opt_low == "wait":
+    door=input("Which door would you like to pass through: \n"
+              "'Red', 'Yellow' or 'Blue'? \n")
+    door_low=door.lower()
+    if door_low == "red":
+      print("You have entered a room full of fire. GAME OVER!\n")
+    elif door_low == "blue":
+      print("You have entered a flooding room. GAME OVER!\n")
+    elif door_low == "yellow":
+      print("Congratulations! You have WON!\n")
+  elif opt_low == "swim":
+    print("You have been electrocuted by an eel. GAME OVER!\n")
+elif dir_low == "right":
+  print("You have been caught by pirates. GAME OVER!\n")
