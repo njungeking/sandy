34f9000fa4d665b4220d9fee51dcf34377a58bdc
Kingsley Mwaura <njungeking@gmail.com>
2024-01-27 10:23:17 +0300

    Summation using the range function

---
+++ Python_Projects/Password-gen/sum-range.py
@@ -0,0 +1,9 @@
+# Total sum of 0 - 100 project
+# using the range function.
+
+print("Welcome to a summation program!\n")
+
+total = 0
+for x in range(0,101):
+  total += x
+print(f"The total sum is: {total}!\n")
