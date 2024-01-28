7195c6271eb847ade97abd10a6bcb735e896382a
Kingsley Mwaura <njungeking@gmail.com>
2024-01-28 18:58:09 +0300

    An introduction into functions

---
+++ Python_Projects/Functions/reeborg1.py
@@ -0,0 +1,28 @@
+# Reeborg jump project.
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+move()
+jump()
+move()
+jump()
+move()
+jump()
+move()
+jump()
+move()
+jump()
+move()
+jump()
