bde78bf3e9ad34c52d60a5692d22d2234a7b021b
Kingsley Mwaura <njungeking@gmail.com>
2024-01-30 17:54:26 +0300

    Second attempt at the hurdles 3 project

---
+++ Python_Projects/Functions/hurdles3-2.py
@@ -0,0 +1,21 @@
+# Another attempt at Reeborg hurdles 3 project.
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    else:
+        jump()
