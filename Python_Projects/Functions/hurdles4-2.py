a96aa68d49ef816a2406bf44018a2322089c741e
Kingsley Mwaura <njungeking@gmail.com>
2024-01-31 21:17:04 +0300

    A second attempt at the reeborg's world 4 project

---
+++ Python_Projects/Functions/hurdles4-2.py
@@ -0,0 +1,25 @@
+# A second attempt at the Reeborg's World 4
+# project involving the use of while loops and
+# conditional statements.
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
