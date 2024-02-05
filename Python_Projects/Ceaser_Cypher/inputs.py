a2c8ab69838e2ab70a5a25154c4462fb25187e6c
Kingsley Mwaura <njungeking@gmail.com>
2024-02-05 19:38:46 +0300

    Functions with inputs

---
+++ Python_Projects/Ceaser_Cypher/inputs.py
@@ -0,0 +1,8 @@
+# Functions with inputs(Parameters and Arguments).
+
+def greet_and_name(name):
+  print(f"Hello {name}!")
+  print(f"How are you {name}?")
+  print(f"Good to see you {name}!")
+
+greet_and_name("Matilda")
