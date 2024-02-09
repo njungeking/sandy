8967acdf1d997db1f304b2bcbb58bf4e7181668c
Kingsley Mwaura <njungeking@gmail.com>
2024-02-09 16:32:59 +0300

    Ceaser function in one

---
+++ Python_Projects/Ceaser_Cypher/Ceaser_func1.py
@@ -0,0 +1,21 @@
+# The use of a single function for the Ceaser-Cypher project.
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+
+print("Welcome to a Ceaser Cypher program!\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def ceaser(prints,move,opt):
+  word = ""
+  if opt == "decode":
+    move *= -1
+  for let in prints:
+    pos = alphabet.index(let)
+    new_pos = pos + move
+    new_let = alphabet[new_pos]
+    word += new_let
+  print(f"The {opt}d message is {word}\n")
+
+ceaser(prints=text,move=shift,opt=direction)
