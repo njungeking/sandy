84411b59c122d2d06d1e30b6e911de54815bde93
Kingsley Mwaura <njungeking@gmail.com>
2024-02-13 11:09:20 +0300

    Lesson #2 on nested dictionaries

---
+++ Python_Projects/Dictionaries/nested_dictionaries2.py
@@ -0,0 +1,8 @@
+# Lesson #2 on the nested dictionaries.
+
+travel_vlog = {
+"France": {"cities_visited": ["Bordeux", "Paris", "Dijon"], "Hours_spent": 12},
+"Germany": {"Major_cities": ["Berlin", "Cologne", "Koln"], "Kms": [12, "hundred", 23], "People": ["Poles", "Slavs", "Bavarians"]},
+}
+
+print(travel_vlog)
