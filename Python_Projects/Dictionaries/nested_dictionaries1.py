8b7772da342f5aeacd1ee26d62b81345f71059ca
Kingsley Mwaura <njungeking@gmail.com>
2024-02-13 10:52:47 +0300

    Nested dictionaries

---
+++ Python_Projects/Dictionaries/nested_dictionaries1.py
@@ -0,0 +1,10 @@
+# A lesson on dictionaries containing nested dictionaries and lists.
+
+travel_vlog = {
+"Kenya": ["Nairobi", "Mombasa", "Eldoret", "Kisumu"],
+"Hours": 12,
+"People": {"Tribes": ["Kisii", "Maragoli", "Kikuyu"]},
+"Uganda": {"Cities": ["Kampala", "Jinja", "Busia"]},
+}
+
+print(travel_vlog)
