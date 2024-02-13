528b388c7bac6da8bbd6bd4ee6b1bb351c8ff4fc
Kingsley Mwaura <njungeking@gmail.com>
2024-02-13 11:20:09 +0300

    Lists containing dictionaries

---
+++ Python_Projects/Dictionaries/lists-dictionaries.py
@@ -0,0 +1,17 @@
+# Lists containing dicitonaries.
+
+travel_vlog = [
+{
+"Country": "France",
+"cities_visited": ["Bordeux", "Paris", "Dijon"], 
+"Hours_spent": 12,
+},
+{
+"Country": "Germany",
+"Major_cities": ["Berlin", "Cologne", "Koln"],
+"Kms": [12, "hundred", 23], 
+"People": ["Poles", "Slavs", "Bavarians"]
+}
+]
+
+print(travel_vlog[1])
