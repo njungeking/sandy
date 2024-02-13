b4873bfaddc5f67b36421b1ce0ca640229fe741f
Kingsley Mwaura <njungeking@gmail.com>
2024-02-13 11:28:08 +0300

    Lists containing dictionaries

---
+++ Python_Projects/Dictionaries/list2.py
@@ -0,0 +1,19 @@
+# Attempt #2 on lists containing dictionaries.
+
+animals = [
+{
+  "Habitat": "Domestic",
+  "Pets": ["Cat", "Dog", "Parrot"],
+  "Subsistence": ["Pig", "Cow", "Goat", "Sheep"]
+},
+{
+  "Habitat": "Wild",
+  "Carnivores": ["Lions", "Cheetahs", "Leopards", "Jackals"],
+  "Herbivores": ["Gazzelles", "Buffaloes", "Hippos"],
+  "Population": 1232,
+}
+]
+
+print(animals[0])
+print(animals[1])
+print(animals)
