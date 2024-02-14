eb91b69b0affc6760092ecda5af44c977e73b746
Kingsley Mwaura <njungeking@gmail.com>
2024-02-14 13:13:13 +0300

    A recap of lists that contain nested dictionaries

---
+++ Python_Projects/Dictionaries/reco-lists.py
@@ -0,0 +1,18 @@
+# A recap of lists containing dictionaries.
+
+travel_log = [
+{
+  "country": "Kenya",
+  "cities": ["Nairobi", "Mombasa", "Nakuru", "Kisumu"],
+  "counties": 47,
+},
+{
+  "country": "France",
+  "cities": ["Paris", "Dijon", "Lens"],
+  "counties": 12
+}
+]
+
+print(travel_log)
+print(travel_log[0])
+print(travel_log[1])
